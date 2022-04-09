99b3a4e94b7412a3561100fea83c839a9637ac17
Zolika56 <66462036+Zolika56@users.noreply.github.com>
2022-04-09 16:44:36 +0200

    Update event.js

--- weblap/event.js
+++ weblap/event.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     let img=document.createElement("img");
+    let ide=document.getElementById("ide");
     $(img).css("width","100%");
+    $(ide).css("padding-top","45px");
     $(img).css("min-width","320px");
     $(img).css("min-height","155px");
     $(img).css("height","100%");
     $(img).css("border","2px solid");
     $(img).css("border-image","linear-gradient(45deg,rgb(55, 217, 168), rgb(25, 92, 89)) 10");
-    let ide=document.getElementById("ide");
+   
     ide.append(img);
     let images=['images/pictures/1.png','images/pictures/2.png','images/pictures/3.png','images/pictures/4.png','images/pictures/5.png','images/pictures/6.png','images/pictures/7.png','images/pictures/8.png'];
     setInterval(function(){
