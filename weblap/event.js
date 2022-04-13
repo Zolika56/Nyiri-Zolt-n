1af2faea34eeb624ecf4b583384e39d17a0cd8d6
Zolika56 <66462036+Zolika56@users.noreply.github.com>
2022-04-13 20:42:38 +0200

    Update event.js

--- weblap/event.js
+++ weblap/event.js
@@ -7,12 +7,11 @@ $(document).ready(function () {
     $(img).css("min-height","155px");
     $(img).css("height","100%");
     $(img).css("border","2px solid");
+    $(img).css("margin-bottom","10%");
     $(img).css("border-image","linear-gradient(45deg,rgb(55, 217, 168), rgb(25, 92, 89)) 10");
    
     ide.append(img);
-    let images=['pictures/1.png','pictures/2.png','pictures/3.png','pictures/4.png','pictures/5.png','pictures/6.png','pictures/7.png','pictures/8.png'];
-    setInterval(function(){
+   
     let random=Math.floor(Math.random()*8);
-    img.src=images[random];
-},500);
+    img.src='images/astronauts.gif';
 });
